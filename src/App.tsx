f269d2390644f65c49e1cd09933229f85ecc2c09
dev.arthur <contato.arthurreis222@gmail.com>
2024-07-01 16:59:25 -0300

    feat: add react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
+import { BrowserRouter } from 'react-router-dom';
+import { AppRoutes } from './routes';
+
 export const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
+  );
+};
